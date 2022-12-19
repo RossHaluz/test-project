f3664ae27cd637df473fb5f91c21db7eeac6f7db
Rostyslav <rossgaluzinskiy@gmail.com>
2022-12-19 14:46:24 +0200

    Update pagination.js

--- src/js/paginatin/pagination.js
+++ src/js/paginatin/pagination.js
@@ -3,40 +3,9 @@ import 'tui-pagination/dist/tui-pagination.css';
 import FetchFilmsApi from '../fetch-service/fechFilmsApi';
 import refs from "../fetch-service/refs";
 
+
 export function creatPagination() {
-    const options = {
-  totalItems: 10,
-  itemsPerPage: 10,
-  visiblePages: 10,
-  centerAlign: false,
-  firstItemClassName: 'tui-first-child',
-  lastItemClassName: 'tui-last-child',
-  template: {
-    page: '<a href="#" class="tui-page-btn">{{page}}</a>',
-    currentPage: '<strong class="tui-page-btn tui-is-selected">{{page}}</strong>',
-    moveButton:
-      '<a href="#" class="tui-page-btn tui-{{type}}">' +
-        '<span class="tui-ico-{{type}}">{{type}}</span>' +
-      '</a>',
-    disabledMoveButton:
-      '<span class="tui-page-btn tui-is-disabled tui-{{type}}">' +
-        '<span class="tui-ico-{{type}}">{{type}}</span>' +
-      '</span>',
-    moreButton:
-      '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip">' +
-        '<span class="tui-ico-ellip">...</span>' +
-      '</a>'
-  }
-};
+    const instance = new Pagination(refs.pagination)
 
-
-  const pagination = new Pagination(refs.pagination, options);
-
-//   if (visiblePages > 1) {
-//     refs.pagination.style.display = 'block';
-//   } else {
-//     refs.pagination.style.display = 'none';
-//   }
-
-  return pagination;
+instance.getCurrentPage()
 }
