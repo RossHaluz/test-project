50ea100b17e9cb48fd8b6dca696019fa1e54c1e9
Rostyslav <rossgaluzinskiy@gmail.com>
2022-12-19 11:50:53 +0200

    add

--- src/js/main/renderMarkupModal.js
+++ src/js/main/renderMarkupModal.js
@@ -13,20 +13,22 @@ export function renderMarkupModal({ poster_path, title, vote_average, vote_count
       height="500"
       class="modal__img"
     />
+    <div class="modal-cover">
     <h2 class="modal__title">${title}</h2>
-    <ul class="modal__list">
+    <ul class="modal__list list">
       <li class="modal__list--item">Vote / Votes</li>
       <li class="modal__list--item">Popularity</li>
       <li class="modal__list--item">Original Title</li>
       <li class="modal__list--item">Genre</li>
     </ul>
 
-    <ul class="moda__option">
+    <ul class="moda__option list">
       <li class="modal__option--item">${vote_average.toFixed(1)} / ${vote_count}</li>
       <li class="modal__option--item">${popularity}</li>
       <li class="modal__option--item">${title}</li>
       <li class="modal__option--item">SCIENCE FICTION</li>
     </ul>
+    </div>
 
     <h3 class="modal__about">ABOUT</h3>
     <p class="modal__about--text">
