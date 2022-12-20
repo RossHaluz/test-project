c4b4148a910c06710c45728a8bb363f98ad8f486
Rostyslav <rossgaluzinskiy@gmail.com>
2022-12-21 01:52:27 +0200

    add

--- src/js/fetch-service/refs.js
+++ src/js/fetch-service/refs.js
@@ -8,6 +8,8 @@ const refs = {
     closeBtn: document.querySelector('.modal-close-btn'),
     pagination: document.getElementById('pagination'),
     modalWrapp: document.querySelector('.modal-wrapp'),
+    modalBtnWatched: document.querySelector('.modal__btn--watched'),
+    modalBtnQueue: document.querySelector('.modal__btn--queue'),
 }
 
 export default refs;
